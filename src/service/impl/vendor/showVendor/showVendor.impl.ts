12a8c0ae2d67f73710c5127c5201958f951d94bc
asohyannick <asohyannick3@gmail.com>
2025-07-01 14:02:46 -0700

    Admin can now fetch and view a single vendor created on our backend REST API successfully

---
+++ src/service/impl/vendor/showVendor/showVendor.impl.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Vendor from "../../../../model/vendor/vendor.model";
+const showVendor = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const vendor = await Vendor.findById(id);
+        if (!vendor) {
+            return res.status(StatusCodes.NOT_FOUND).json({message: "Vendor doesn't exist!"});
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Vendor has been fetched successfully!",
+            vendor
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+
+export default showVendor;
