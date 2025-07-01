03acf3a99063d7ad8bf7aa849cc1b11833e1fd83
asohyannick <asohyannick3@gmail.com>
2025-07-01 02:22:22 -0700

    User can now removed permanently his or her profile account successfully because this API endpoint has been created and implemented successfully on our NodeMultiVendorMarketPlace backend or REST API

--- src/controller/profile/profile.controller.ts
+++ src/controller/profile/profile.controller.ts
@@ -6,9 +6,11 @@ import createProfile from '../../service/impl/profile/createProfile/createProfil
 import showProfiles from '../../service/impl/profile/showProfiles/showProfiles.impl';
 import showProfile from '../../service/impl/profile/showProfile/showProfile.impl';
 import updateProfile from '../../service/impl/profile/updateProfile/updateProfile.impl';
+import deleteProfile from '../../service/impl/profile/deleteProfile/deleteProfile.impl';
 const router = express.Router();
 router.post('/create-profile-account', authenticationToken, globalValidator(validateProfileRegistration), createProfile);
 router.get('/show-profiles', authenticationToken, showProfiles);
 router.get('/show-profile/:id', authenticationToken, showProfile);
 router.put('/update-profile/:id', authenticationToken, globalValidator(validateUpdatedProfileRegistration), updateProfile);
+router.delete('/delete-profile/:id', authenticationToken, deleteProfile);
 export default router;
