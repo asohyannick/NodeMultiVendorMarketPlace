a1c1a1f9faf1cbaa98d7837c540b4d515c421953
asohyannick <asohyannick3@gmail.com>
2025-07-01 02:14:48 -0700

    Admin can now fetch and view a single user's profile account successfully because this API endpoint has been created on our NodeMultiVendorMarketPlace backend or REST API

---
+++ src/service/impl/profile/showProfile/showProfile.impl.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import User from "../../../../model/user/user.model";
+const showProfile = async(req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.body;
+        const profile = await User.findById(id);
+        if (!profile) {
+            return res.status(StatusCodes.NOT_FOUND).json({message: "Profile doesn't exist!"});
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "User profile has been fetched successfully!",
+            profile
+        })
+    } catch (error) {
+        console.error("Error occurred!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+
+export default showProfile;
