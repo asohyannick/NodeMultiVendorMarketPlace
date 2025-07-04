d1cf41622c6bd8fa15e9c557340ff7fa3f1787f6
asohyannick <asohyannick3@gmail.com>
2025-07-04 13:51:21 -0700

    User can now edit and update a single  payment history he or she has processed and made with stripe successfully on our application because this API endpoint has been created successfully!

--- src/controller/stripe/stripe.controller.ts
+++ src/controller/stripe/stripe.controller.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import authenticationToken from '../../middleware/auth/authenticationToken.middleware';
 import { processStripePayment } from '../../service/impl/stripe/processStripePayment/processStripePayment.impl';
 import globalValidator from '../../utils/globalValidator';
-import { validateStripePayment } from '../../utils/validator';
+import { validateStripePayment, validateUpdatedStripePayment } from '../../utils/validator';
 import { showStripePayments } from '../../service/impl/stripe/showStripePayments/showStripePayments.impl';
 import { showStripePayment } from '../../service/impl/stripe/showStripePayment/showStripePayment.impl';
+import { editAndUpdateStripePayment } from '../../service/impl/stripe/updateStripePayment/updateStripePayment.impl';
 const router = express.Router();
 router.post('/create-payment', authenticationToken, globalValidator(validateStripePayment), processStripePayment);
 router.get('/show-payments', authenticationToken, showStripePayments);
 router.get('/show-payment/:id', authenticationToken, showStripePayment);
+router.put('/show-payment/:id', authenticationToken, globalValidator(validateUpdatedStripePayment), editAndUpdateStripePayment);
+
 export default router;
