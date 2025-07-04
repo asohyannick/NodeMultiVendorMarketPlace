b2c13d0d16b7c1457f5b68f57b9e1e15eb8a8b43
asohyannick <asohyannick3@gmail.com>
2025-07-04 13:54:24 -0700

    User can now removed permanently or delete a single payment history he or she has processed and made with stripe successfully on our application because this API endpoint has been created successfully!

--- src/controller/stripe/stripe.controller.ts
+++ src/controller/stripe/stripe.controller.ts
@@ -6,10 +6,11 @@ import { validateStripePayment, validateUpdatedStripePayment } from '../../utils
 import { showStripePayments } from '../../service/impl/stripe/showStripePayments/showStripePayments.impl';
 import { showStripePayment } from '../../service/impl/stripe/showStripePayment/showStripePayment.impl';
 import { editAndUpdateStripePayment } from '../../service/impl/stripe/updateStripePayment/updateStripePayment.impl';
+import { deleteStripePayment } from '../../service/impl/stripe/deleteStripePayment/deleteStripePayment.impl';
 const router = express.Router();
 router.post('/create-payment', authenticationToken, globalValidator(validateStripePayment), processStripePayment);
 router.get('/show-payments', authenticationToken, showStripePayments);
 router.get('/show-payment/:id', authenticationToken, showStripePayment);
 router.put('/show-payment/:id', authenticationToken, globalValidator(validateUpdatedStripePayment), editAndUpdateStripePayment);
-
+router.delete('/delete-payment/:id', authenticationToken, deleteStripePayment);
 export default router;
