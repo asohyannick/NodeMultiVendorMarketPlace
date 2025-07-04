ef4db2c3159f773f04ce48d4408c00c8241fa243
asohyannick <asohyannick3@gmail.com>
2025-07-04 14:55:22 -0700

    User can now delete or removed permanently a single available product review created and submitted to us successfully on our application because this API endpoint has been created and implemented successfully on our backend!

--- src/controller/review/review.controller.ts
+++ src/controller/review/review.controller.ts
@@ -6,9 +6,11 @@ import createReview from '../../service/impl/review/createReview/createReview.im
 import showReviews from '../../service/impl/review/showReviews/showReviews.impl';
 import showReview from '../../service/impl/review/showReview/showReview.impl';
 import updateReview from '../../service/impl/review/updateReview/updateReview.impl';
+import deleteReview from '../../service/impl/review/deleteReview/deleteReview.impl';
 const router = express.Router();
 router.post('/create-review', authenticationToken, globalValidator(reviewValidationSchema), createReview);
 router.get('/show-reviews', authenticationToken, showReviews);
 router.get('/show-review/:id', authenticationToken, showReview);
 router.put('/update-review/:id', authenticationToken, globalValidator(updateReviewValidationSchema), updateReview);
+router.delete('/delete-review/:id', authenticationToken, deleteReview);
 export default router;
---
+++ src/service/impl/review/deleteReview/deleteReview.impl.ts
@@ -0,0 +1,25 @@
+import Review from "../../../../model/review/review.model";
+import { Request, Response } from 'express';
+import { StatusCodes } from "http-status-codes";
+const deleteReview = async(req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const review = await Review.findByIdAndDelete(id);
+        if (!review) {
+            return res.status(StatusCodes.NOT_FOUND).json({message: "Product review doesn't exist!"});
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Product review has been deleted successfully!",
+            review,
+        })
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+
+export default deleteReview;
