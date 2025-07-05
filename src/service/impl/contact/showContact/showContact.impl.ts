b7f44fd2e8466e0c8fe479543ba39dff84cf3db2
asohyannick <asohyannick3@gmail.com>
2025-07-04 17:14:22 -0700

    User can now fetch and view a single availabe message send to the admin successfully on our application

---
+++ src/service/impl/contact/showContact/showContact.impl.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Contact from "../../../../model/contact/contact.model";
+const showContact = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const contact = await Contact.findById(id);
+        if (!contact) {
+            return res.status(StatusCodes.NOT_FOUND).json({message: "Contact doesn't exist!"});
+        }
+        return res.status(StatusCodes.OK).json({
+            message: "Contact message has been retrieved successfully!",
+            contact,
+        });
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        })
+    }
+}
+
+export default showContact;
